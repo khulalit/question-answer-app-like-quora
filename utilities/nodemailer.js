cc4ffab889351f7c544790d73b07920de689b757
Lalit <52879143+khulalit@users.noreply.github.com>
2022-07-25 14:01:41 +0530

    Update nodemailer.js

--- utilities/nodemailer.js
+++ utilities/nodemailer.js
@@ -5,29 +5,10 @@ const transporter = nodemailer.createTransport(
     {
         service: 'gmail',
         auth:{
-            user: 'lalitkhudhania2@gmail.com',
-            pass: 'tgzglpnzyuaejxaw'
+            user: process.env.EMAIL,
+            pass: process.env.PASS
         }
     }
 );
 
-// const mailOptions = {
-//     from: '"Admin do-not reply" <lalitkhudhania2@gmail.com>', // sender address
-//     to: 'lalitkhudhania1@gmail.com', // list of receivers
-//     subject: 'Welcome!',
-//     template: 'email', // the name of the template file i.e email.handlebars
-//     context:{
-//         name: "Adebola", // replace {{name}} with Adebola
-//         company: 'My Company' // replace {{company}} with My Company
-//     }
-
-// };
-
-// // trigger the sending of the E-mail
-// transporter.sendMail(mailOptions, function(error, info){
-//     if(error){
-//         return console.log(error);
-//     }
-//     console.log('Message sent: ' + info.response);
-// });
 module.exports = transporter;
